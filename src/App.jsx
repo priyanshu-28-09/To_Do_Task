c3b0f5d16027deafc01961c876d8a2c78bbf5a5b
priyanshu vishwakarma <priyanshuvishwakarma3133@gmail.com>
2026-05-28 15:14:55 +0530

    react app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Home from "./Home";
+
+function App() {
+
+  return (
+
+    <div className="app-container">
+
+      <Home />
+
+    </div>
+  );
+}
+
+export default App;
